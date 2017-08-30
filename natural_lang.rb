0ff60f48c046040a16fad15cbaa354e62856b4a2
Diwakar Chand <dchand@covermymeds.com>
2017-08-30 11:55:17 -0400

    translate sample' '

--- natural_lang.rb
+++ natural_lang.rb
@@ -1,26 +1,30 @@
 # Imports the Google Cloud client library
 require "google/cloud/language"
 require 'pry-byebug'
+begin
+  # Instantiates a client
+  language = Google::Cloud::Language.new(
+      project: "absolute-range-177915",
+      keyfile: "./access_key.json")
 
-# Instantiates a client
-language = Google::Cloud::Language.new(
-    project: "absolute-range-177915",
-    keyfile: "./access_key.json")
+  texts = {
+      :neutral => "The lines are long in walmart on Sunday's." ,
+      :neutral_factual => "China and India has more than two billion in population." ,
+      :positve => "I had a great time with my friends and family last weekend!" ,
+      :sarcasm => "Oh! What a beauty! The life I have created for myself." ,
+      :negative => "I had to wait for eight long fuc*** hours in line with no water and hot and humid weather."
+  }
 
-texts = {
-    :neutral => "The lines are long in walmart on Sunday's." ,
-    :neutral_factual => "China and India has more than two billion in population." ,
-    :positve => "I had a great time with my friends and family last weekend!" ,
-    :sarcasm => "Oh! What a beauty! The life I have created for myself." ,
-    :negative => "I had to wait for eight long fuc*** hours in line with no water and hot and humid weather."
-}
-
-texts.each do |t|
-  text = language.document t[1]
-  sentiment = text.sentiment
-  puts text.annotate
-  STDOUT.write "#{t[0]} | "
-  STDOUT.write "#{t[1]} | "
-  STDOUT.write "#{sentiment.score} | "
-  STDOUT.write "#{sentiment.magnitude}\n\n"
- end
+  texts.each do |t|
+    text = language.document t[1]
+    sentiment = text.sentiment
+    puts text.annotate
+    STDOUT.write "#{t[0]} | "
+    STDOUT.write "#{t[1]} | "
+    STDOUT.write "#{sentiment.score} | "
+    STDOUT.write "#{sentiment.magnitude}\n\n"
+end
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
+end
--- translate.rb
+++ translate.rb
@@ -1,26 +1,25 @@
 # Imports the Google Cloud client library
-require "google/cloud/language"
+require "google/cloud/translate"
 require 'pry-byebug'
+begin
+  # Instantiates a client
+  translate = Google::Cloud::Translate.new(
+      project: "absolute-range-177915",
+      keyfile: "./access_key.json")
 
-# Instantiates a client
-language = Google::Cloud::Language.new(
-    project: "absolute-range-177915",
-    keyfile: "./access_key.json")
+  puts "Supported languages:"
+  translate.languages.each do |language|    
+    puts "#{language.code} #{language.name}"
+  end
 
-texts = {
-    :neutral => "There are huge lines at the walmart." ,
-    :neutral_factual => "China and India has more than two billion in population." ,
-    :positve => "I had a great time with my friends and family last weekend!" ,
-    :sarcasm => "Oh! What a beauty! The life I have created for myself." ,
-    :negative => "I had to wait for eight long fuc*** hours in line with no water and hot and humid weather."
-}
+  detections = translate.detect "chien", "chat", "bol", "hola"
+  detections.each do |d|
+    STDOUT.write "#{d.text} | "
+    STDOUT.write "#{d.language} | "
+    STDOUT.write "#{d.confidence} | \n"
+  end
 
-texts.each do |t|
-  text = language.document t[1]
-  sentiment = text.sentiment
-  puts text.annotate
-  STDOUT.write "#{t[0]} | "
-  STDOUT.write "#{t[1]} | "
-  STDOUT.write "#{sentiment.score} | "
-  STDOUT.write "#{sentiment.magnitude}\n\n"
- end
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
+end
